c9e086e6e828bb255dce45746b400bbe3a85eca8
goro-cat <goro2.8223@gmail.com>
2021-08-29 06:46:48 +0000

    model-fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,120 +10,122 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_818_123_223) do
-  create_table 'chats', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'partner_id'
-    t.text 'message'
-    t.string 'chat_image_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema.define(version: 2021_08_18_123223) do
+
+  create_table "chats", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "partner_id"
+    t.text "message"
+    t.string "chat_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'comments', force: :cascade do |t|
-    t.integer 'post_id'
-    t.integer 'user_id'
-    t.text 'comment', default: '', null: false
-    t.string 'comment_image_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.text "comment", null: false
+    t.string "comment_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'favorites', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'post_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'genres', force: :cascade do |t|
-    t.integer 'post_id'
-    t.integer 'category', default: 0, null: false
-    t.integer 'animal', default: 0, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "genres", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "category", default: 0, null: false
+    t.integer "animal", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'maps', force: :cascade do |t|
-    t.integer 'post_id'
-    t.integer 'comment_id'
-    t.string 'address'
-    t.float 'latitude'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "maps", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "comment_id"
+    t.string "address"
+    t.float "latitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'notifications', force: :cascade do |t|
-    t.integer 'visiter_id'
-    t.integer 'visited_id'
-    t.integer 'post_id'
-    t.integer 'comment_id'
-    t.integer 'chat_id'
-    t.string 'action', default: '', null: false
-    t.boolean 'cheacked', default: false, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "notifications", force: :cascade do |t|
+    t.integer "visiter_id"
+    t.integer "visited_id"
+    t.integer "post_id"
+    t.integer "comment_id"
+    t.integer "chat_id"
+    t.string "action", default: "", null: false
+    t.boolean "cheacked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'post_images', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'post_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'image_id'
-    t.index ['post_id'], name: 'index_post_images_on_post_id'
+  create_table "post_images", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_id"
+    t.index ["post_id"], name: "index_post_images_on_post_id"
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.integer 'user_id'
-    t.text 'text'
-    t.string 'plase'
-    t.string 'pet_name'
-    t.integer 'pet_sex'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'category'
-    t.integer 'animal'
-    t.index ['user_id'], name: 'index_posts_on_user_id'
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "text"
+    t.string "plase"
+    t.string "pet_name"
+    t.integer "pet_sex"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "category"
+    t.integer "animal"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table 'relationships', force: :cascade do |t|
-    t.integer 'follower_id'
-    t.integer 'followed_user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'rooms', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'user_rooms', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'room_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "user_rooms", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'last_name'
-    t.string 'first_name'
-    t.string 'kana_last_name'
-    t.string 'kana_first_name'
-    t.string 'account_name'
-    t.string 'account_image_id'
-    t.string 'address'
-    t.string 'phone_number'
-    t.text 'introduction'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "kana_last_name"
+    t.string "kana_first_name"
+    t.string "account_name"
+    t.string "account_image_id"
+    t.string "address"
+    t.string "phone_number"
+    t.text "introduction"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
